910674f3354ff03c5441bbcde9b0cced7953bfec
Jon Robeson <jcbrobeson@gmail.com>
2025-01-20 11:52:25 -0600

    basic form for adding goals for courses

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,11 +1,35 @@
-export default function CourseGoal() {
+import { type PropsWithChildren } from 'react';
+
+type CourseGoalProps = PropsWithChildren<{ title: string; id: number; onDelete: (id: number) => void }>;
+
+//or
+// interface CourseGoalProps {
+// 	title: string;
+// children: ReactNode
+// }
+
+export default function CourseGoal({ title, id, onDelete, children }: CourseGoalProps) {
 	return (
 		<article>
 			<div>
-				<h2>TITLE</h2>
-				<p></p>
+				<h2>{title}</h2>
+				{children}
 			</div>
-			<button>DELETE</button>
+			<button onClick={() => onDelete(id)}>DELETE</button>
 		</article>
 	);
 }
+/// or
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+// 	return (
+// 		<article>
+// 			<div>
+// 				<h2>{title}</h2>
+// 				{children}
+// 			</div>
+// 			<button>DELETE</button>
+// 		</article>
+// 	);
+// };
+
+// export default CourseGoal;
